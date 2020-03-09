b8c373b21b193bba14d492c23de2110aff8f295c
Sam Deane <sam@elegantchaos.com>
2020-03-09 15:21:29 +0000

    Fixed header.

--- Client/SparkleBridgeClient.h
+++ Client/SparkleBridgeClient.h
@@ -2,7 +2,7 @@
 //  SparkleBridgeFramework.h
 //  SparkleBridgeFramework
 //
-//  Created by Developer on 02/03/2020.
+//  Created by Sam Deane on 02/03/2020.
 //  Copyright © 2020 Elegant Chaos. All rights reserved.
 //
 
